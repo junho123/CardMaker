7e63a724e0a7a9d812e0d35f0dc85efabd7a8283
junho123 <wn531300@gmail.com>
2022-01-03 13:54:46 +0900

    project set up & add firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+class AuthService {
+  login(providerName) {
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider); // 프로미스로 리턴
+  }
+}
+
+export default AuthService;
